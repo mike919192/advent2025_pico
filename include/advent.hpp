8bb24fc10910898fd43eebee7cf39261f1e13206
Mike Bloom <mbloom@mac-ndt.com>
2025-12-19 21:25:07 -0500

    Remove overload

--- include/advent.hpp
+++ include/advent.hpp
@@ -6,21 +6,7 @@ namespace advt
 {
 
 template <size_t n>
-bool getline(etl::string<n> &line)
-{
-    line.clear();
-    int read_char{ 0 };
-    while (read_char = getchar(), read_char != EOF && read_char != '\x1A' && read_char != '\n') {
-        line.append(1, static_cast<char>(read_char));
-    }
-
-    if (read_char == EOF || read_char == '\x1A')
-        return false;
-    return true;
-}
-
-template <size_t n>
-bool getline(etl::string<n> &line, char delim)
+bool getline(etl::string<n> &line, char delim = 0)
 {
     line.clear();
     int read_char{ 0 };
